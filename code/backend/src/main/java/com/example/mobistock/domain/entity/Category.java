e9e6f3acd5c9c92b82bea8e8cca22cdc2ba115ee
BB <sorawitthassadorn15@gmail.com>
2026-09-12 19:47:41 +0700

    feat: create domain and dto of Category

---
+++ code/backend/src/main/java/com/example/mobistock/domain/entity/Category.java
@@ -0,0 +1,41 @@
+package com.example.mobistock.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "CATEGORY")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Integer categoryId;
+
+    @Column(name = "category_name_th", length = 255)
+    private String categoryNameTh;
+
+    @Column(name = "category_name_en", length = 255)
+    private String categoryNameEn;
+
+    @CreationTimestamp
+    @Column(name = "create_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "update_at")
+    private LocalDateTime updatedAt;
+}
